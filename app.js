82ce61709f34a1cb6271d3b0441967c6e2b1ef1a
Chamitha <chamithashelan@gmail.com>
2024-02-04 07:46:22 +0530

    Created a function to play a round.
    Created the logic to play one round.

--- app.js
+++ app.js
@@ -16,4 +16,25 @@ function getComputerChoice(){
     return choice;
 }
 
-console.log(getComputerChoice());
+function playRound(playerChoice, computerChoice ){
+    if(playerChoice.toLowerCase() == computerChoice.toLowerCase()){
+        return "It's a tie ! " ;
+    }else if(playerChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock"){
+        return "You Win! Paper beats Rock!";
+    }else if(playerChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissor"){
+        return "You Win! Rock beats Scissor!";
+    }else if(playerChoice.toLowerCase() == "scissor" && computerChoice.toLowerCase() == "paper"){
+        return "You Win! Scissor beats Paper!";
+
+    }else if(computerChoice.toLowerCase() == "paper" && playerChoice.toLowerCase() == "rock"){
+        return "You Lose! Paper beats Rock!";
+    }else if(computerChoice.toLowerCase() == "rock" && playerChoice.toLowerCase() == "scissor"){
+        return "You Lose! Rock beats Scissor!";
+    }else if(computerChoice.toLowerCase() == "scissor" && playerChoice.toLowerCase() == "paper"){
+        return "You Lose! Scissor beats Paper !";
+    
+    }else{
+        return "Wrong choice";
+    }
+}
+
