bcdcc8af35787e61ccacea701cfb310141eb3811
Chamitha <chamithashelan@gmail.com>
2024-02-04 08:00:14 +0530

    Modified playRound(). Added getPlayerChoice() and added playGame() functions.
    getPlayerChoice() will prompt the user for a choice
    and return either RPS. playGame() will loop playRound()
    five times.

--- app.js
+++ app.js
@@ -17,20 +17,20 @@ function getComputerChoice(){
 }
 
 function playRound(playerChoice, computerChoice ){
-    if(playerChoice.toLowerCase() == computerChoice.toLowerCase()){
+    if(playerChoice.toLowerCase() === computerChoice.toLowerCase()){
         return "It's a tie ! " ;
-    }else if(playerChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock"){
+    }else if(playerChoice.toLowerCase() === "paper" && computerChoice.toLowerCase() === "rock"){
         return "You Win! Paper beats Rock!";
-    }else if(playerChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissor"){
+    }else if(playerChoice.toLowerCase() === "rock" && computerChoice.toLowerCase() === "scissor"){
         return "You Win! Rock beats Scissor!";
-    }else if(playerChoice.toLowerCase() == "scissor" && computerChoice.toLowerCase() == "paper"){
+    }else if(playerChoice.toLowerCase() === "scissor" && computerChoice.toLowerCase() === "paper"){
         return "You Win! Scissor beats Paper!";
 
-    }else if(computerChoice.toLowerCase() == "paper" && playerChoice.toLowerCase() == "rock"){
+    }else if(computerChoice.toLowerCase() === "paper" && playerChoice.toLowerCase() === "rock"){
         return "You Lose! Paper beats Rock!";
-    }else if(computerChoice.toLowerCase() == "rock" && playerChoice.toLowerCase() == "scissor"){
+    }else if(computerChoice.toLowerCase() === "rock" && playerChoice.toLowerCase() === "scissor"){
         return "You Lose! Rock beats Scissor!";
-    }else if(computerChoice.toLowerCase() == "scissor" && playerChoice.toLowerCase() == "paper"){
+    }else if(computerChoice.toLowerCase() === "scissor" && playerChoice.toLowerCase() === "paper"){
         return "You Lose! Scissor beats Paper !";
     
     }else{
@@ -38,3 +38,18 @@ function playRound(playerChoice, computerChoice ){
     }
 }
 
+function getPlayerChoice(){
+    let playerChoice = prompt("Enter your choice", "[Rock] [Paper] [Scissor] ");
+    return playerChoice;
+}
+
+function playGame(){
+    let count = 1;
+
+    while(count <= 5){
+        console.log(playRound(getPlayerChoice(), getComputerChoice()));
+        count++;
+    }
+}
+
+playGame();
