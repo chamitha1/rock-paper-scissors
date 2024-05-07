dbb3be8b4d9b3e484d6391d7081804fb592e9b24
Chamitha <chamithashelan@gmail.com>
2024-05-07 23:19:57 +0530

    Adding Score System.
    -Not completed yet.
    -score count is bugging. It keeps counting even after
     setting zero
    -Display text.content set isn't working properly.
     Need to manipulate DOM properly

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Functions
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
 
@@ -43,23 +44,76 @@ function getPlayerChoice(){
     return playerChoice;
 }
 
+function scoreCount(result){
+    
+    if(result === 0){ //TIE
+        return;
+    }
+    if(result === 1 ){ //Computer WIN
+        return compScore++;
+    }
+    if(result === 2){ //Player WIN
+        return playerScore++;
+    }
+}
+
+function scoreCheck(compScore, playerScore){
+    if(compScore > 4){
+        display.textContent = "COMPUTER WON THE GAME!!";
+        //reset score
+        compScore = 0;
+        playerScore = 0;
+        score_comp.textContent = 0;
+        score_player.textContent = 0;
+    }
+    if(playerScore > 4){
+        display.textContent = "PLAYER WON THE GAME!!";
+        //reset score
+        compScore = 0;
+        playerScore = 0;
+        score_comp.textContent = 0;
+        score_player.textContent = 0;
+
+    }
+}
+
+//app
+
+let compScore = 0;
+let playerScore = 0;
+
 const btn_rock = document.querySelector("#rock");
 const btn_paper = document.querySelector("#paper");
 const btn_scissors = document.querySelector("#scissor");
 
-const display = document.querySelector(".display");
+const display = document.querySelector("#result");
+const score_comp = document.querySelector("#compScore");
+const score_player = document.querySelector("#playerScore");
+
+const TIE = 0;
+const COMP_WIN = 1;
+const PLAYER_WIN = 2;
+
 
 btn_rock.addEventListener("click" , () =>{
     let result = playRound(btn_rock.id, getComputerChoice());
     switch(result){
         case 0:
             display.textContent = "TIE";
+            scoreCount(0);
+            scoreCheck(compScore, playerScore);
             break;
         case 1:
             display.textContent = "Computer WIN";
+            scoreCount(1);
+            scoreCheck(compScore, playerScore);
+            score_comp.textContent = compScore;
             break;
         case 2:
             display.textContent = "Player WIN";
+            scoreCount(2);
+            scoreCheck(compScore, playerScore);
+            score_player.textContent = playerScore;
             break;
         default:
             console.log("Error");
@@ -70,7 +124,7 @@ btn_paper.addEventListener("click" , () =>{
     let result = playRound(btn_rock.id, getComputerChoice());
     switch(result){
         case 0:
-            display.textContent = "TIE";
+            display.text = "TIE";
             break;
         case 1:
             display.textContent = "Computer WIN";
@@ -100,6 +154,3 @@ btn_scissors.addEventListener("click" , () =>{
     }
 });
 
-const TIE = 0;
-const COMP_WIN = 1;
-const PLAYER_WIN = 2;
